d1a330d78adbbd6cdbe5c090b72d5b807023bde8
Zach Maurer <zach.maurer@gmail.com>
2017-05-29 14:21:15 -0700

    slight modification and bugfixed in feature_extractor, DS_Store ignoring and better naming and json structure

--- feature_extraction/feature_extractor.py
+++ feature_extraction/feature_extractor.py
@@ -11,24 +11,33 @@ Follow the installation instructions on the pyAudioAnalysis github https://githu
 
 Output of stFeatureExtraction is described here https://github.com/tyiannak/pyAudioAnalysis/wiki/3.-Feature-Extraction
 """
-labels = []
-features = []
+labels = {}
+features = {}
 #Should work after running converter.py
 datadir = "../data/processed/"
 frame_size = 0.050
 frame_stepsize = 0.025
-for dirname in os.listdir(datadir):
+print("Frame_size: {},  Step_size: {}".format(frame_size, frame_stepsize))
+total = 0
+for i, dirname in enumerate(os.listdir(datadir)):
+	if dirname == ".DS_Store":
+		continue
 	for filename in os.listdir(datadir + dirname + '/audio_trimmed/pedal/'):
+		if filename == ".DS_Store":
+			continue
 		if "lie" in filename:
-			labels.append(1)
+			#labels.append((filename, 1))
+			labels[filename] = 1
 		else:
-			labels.append(0)
+			labels[filename] = 0
 		[Fs, x] = audioBasicIO.readAudioFile(datadir+dirname+'/audio_trimmed/pedal/'+filename)
 		#we might want to play with the timeframe here - as it is this is giving us up to ~1.5k frames for our sequences
 		st_features = audioFeatureExtraction.stFeatureExtraction(x, Fs, frame_size*Fs, frame_stepsize*Fs)
-		features.append(st_features.tolist())
-
-with open('labels.json', 'w') as label_file:
+		features[filename] = st_features.tolist()
+		total += 1
+	print(i)
+print(total)
+with open('labels_{}_{}.json'.format(frame_size, frame_stepsize), 'w') as label_file:
 	json.dump(labels,label_file)
-with open('extracted_features.json','w') as feature_file:
+with open('extracted_features_{}_{}.json'.format(frame_size, frame_stepsize),'w') as feature_file:
 	json.dump(features,feature_file)
